9c20bdf43bde5c6180ae9231e71ceae3cbe64174
Felipe <felipermpires@gmail.com>
2025-10-07 18:07:32 -0300

    create ocurrence additinal setters

--- src/main/java/central_controle_fogo/com/backend_central_controle_fogo/model/ocurrenceReport/Ocurrence.java
+++ src/main/java/central_controle_fogo/com/backend_central_controle_fogo/model/ocurrenceReport/Ocurrence.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Ocurrence")
@@ -22,29 +23,36 @@ public class Ocurrence extends Base {
     @Column(unique = true, nullable = false, length = 256)
     @NotBlank(message = "Informe o tipo de ocorrência")
     @Size(min = 5, max = 256)
+    @Setter
     private String occurrenceType;
 
     @Column(nullable = false)
     @NotBlank(message = "Insira se existem vítimas")
+    @Setter
     private boolean occurrenceHasVictims;
 
     //Local da ocorrência
 
     @Column(nullable = false, length = 256)
     @NotBlank(message = "Insira o enderaço da ocorrência")
+    @Setter
     private String occurrenceAddress;
 
     @Column(length = 256)
+    @Setter
     private String occurrenceAddressReference;
 
     @Column(length = 100)
+    @Setter
     private String occurrenceLocationType;
 
     @Column(length = 100)
+    @Setter
     private String occurrenceAdditionalRisks;
 
     @Column(nullable = false)
     @NotBlank(message = "Insira se o atendimento é prioritário")
+    @Setter
     private boolean occurrenceIsPriority;
 
     //Dados do solicitante
@@ -55,5 +63,6 @@ public class Ocurrence extends Base {
 
     @Column(nullable = false, length = 11)
     @NotBlank(message = "Insira o telefone para contato do solicitante")
+    @Setter
     private String occurrenceRequesterPhoneNumber;
 }
