3fd7ec8985fb046de937fcd43d0f6b17939e8ba1
braynerdev <dev.brayner@outlook.com>
2025-10-16 20:25:04 -0300

    ajuste conf swagger

--- src/main/java/central_controle_fogo/com/backend_central_controle_fogo/config/OpenApiConfig.java
+++ src/main/java/central_controle_fogo/com/backend_central_controle_fogo/config/OpenApiConfig.java
@@ -11,7 +11,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Minha API")
+                        .title("Central Controle Fogo")
                         .version("1.0")
                         .description("Documentação da API central controle de fogo"));
     }
